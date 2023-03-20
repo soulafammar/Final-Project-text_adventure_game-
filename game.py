02cd83053bd89fff543fb6b0410d2b506b6a4fc8
soulafammar <24sammar@go.dsdmail.net>
2023-03-20 11:03:19 -0600

    forget what i even did here

--- game.py
+++ game.py
@@ -1,3 +1,12 @@
+rooms = {(0,0,0):["Math Class",[],[], ("south"), (), "math class text"],
+		(0,-2,0):["English Class",["hamlet"], [], ("south"), ("grab"), "You enter a classroom full of chattering students. Each student has a laptop open and a copy of the same pale yellow book in hand, discussing it with the others next to them. An abandoned copy lies on a desk in front of you."],
+		"hiddenRoom":["Hidden Room",["key"], [], ("west"), ("grab"), "hidden room text"],
+		(1,0,0):["Hallway outside math class",[], [], ("north","west","east"), (), "You find yourself in a desolate hallway. The overhead lights make the same buzzing sound you've become so accustomed to in math class. There's a faint sound of people conversing to the west, and music playing to the east."],
+		(1,-1,0):["Goldfish Kid",[], ["goldfish kid"],("west","east"), ("use"), "You walk down the hallway. The sound of voices overlapping each other gets louder and louder. goldfish kid here."],
+		(1,-2,0):[[],("west","east","north"),"suddenly you here noise from ANOTHER direction, north, see a door way to english class,"],
+		(1,-3,0):[["vending machine soda"],("west","east"), "here is the vending machine spot"],
+		(1,-4,0):[["escape door"],("north","east"),"south side of cafeteria"],
+		(0,-4,0):[["fairy tales book"],("south"),"were are in the cafeteria, theres popular kids table, and a kid playing video games"]}
 
 print("this is where the intro text will go. Potential commands: grab, use, talk, look, examine, help. Potential movements: up, down, north, south, west, east. ")
 
@@ -12,27 +21,16 @@ help_text = "Potential commands: grab, use, talk, look, examine, help. Potential
 
 print("...")
 print("Your eyelids flutter open, and you wake up from a dream about brown sugar boba (dream is important later) and find yourself in your math class. Forgetting the pleasures of your dream, you take in your surroundings: exhausted 17 year-olds, graphing calculators, papers spread out on uneven desks. Your teacher is droning on about logarithms and their properties. The buzz of the fluorescent ceiling lights is ceaseless. You look towards the door longingly. Your friend sitting besides you catches your glance, and gives you a sympathetic smile. You look back at your teacher and ask to use the restroom. She nods, continuing to describe the change of base properties of both natural and common log.")
-
+print(f"Possible exits: {rooms[tuple(playerPosition)][3]}")
 
 #location of room -> ["name of room",[items],[obstacles],(allowed_movements),(allowed_commands),"flavor text"]
 #dictionary of all the rooms
-rooms = {(0,0,0):["Math Class",[],[], ("south"), (), "math class text"],
-		(0,-2,0):["English Class",["hamlet"], [], ("south"), ("grab"), "You enter a classroom full of chattering students. Each student has a laptop open and a copy of the same pale yellow book in hand, discussing it with the others next to them. An abandoned copy lies on a desk in front of you."],
-		"hiddenRoom":["Hidden Room",["key"], [], ("west"), ("grab"), "hidden room text"],
-		(1,0,0):["Hallway outside math class",[], [], ("north","west","east"), (), "You find yourself in a desolate hallway. The overhead lights make the same buzzing sound you've become so accustomed to in math class. There's a faint sound of people conversing to the west, and music playing to the east."],
-		(1,-1,0):["Goldfish Kid",[], ["goldfish kid"],("west","east"), ("use"), "You walk down the hallway. The sound of voices overlapping each other gets louder and louder. goldfish kid here."],
-		(1,-2,0):[[],("west","east","north"),"suddenly you here noise from ANOTHER direction, north, see a door way to english class,"],
-		(1,-3,0):[["vending machine soda"],("west","east"), "here is the vending machine spot"],
-		(1,-4,0):[["escape door"],("north","east"),"south side of cafeteria"],
-		(0,-4,0):[["fairy tales book"],("south"),"were are in the cafeteria, theres popular kids table, and a kid playing video games"]}
+
 
 #should i do this?
-roomText = {(0,0,0): ["math class text"]
-			(0,-2,0): ["You enter a classroom full of chattering students. Each student has a laptop open and a copy of the same pale yellow book in hand, discussing it with the others next to them. An abandoned copy lies on a desk in front of you.",
-			"You enter a classroom full of chattering students. Each student has a laptop open and a copy of the same pale yellow book in hand, discussing it with the others next to them."]
-			(1,0,0):}
-
-
+#roomText = {(0,0,0): ["math class text"]
+			#(0,-2,0): ["You enter a classroom full of chattering students. Each student has a laptop open and a copy of the same pale yellow book in hand, discussing it with the others next to them. An abandoned copy lies on a desk in front of you."]
+			#}
 def move(direction): #this fucntion doesn't need further restrictions, because the only time it is called is after it passes certain resrictions
 	if direction == "west":
 		playerPosition[1] += -1
@@ -89,8 +87,9 @@ response = input()
 while quitGame == False: #could be a quit variable
 	#get current position variables
 	location_name, items, obstacles, allowed_movements, allowed_commands, description = rooms[tuple(playerPosition)]
-	print(description.get("key", "not valid"))
-	print(f"Possible exits: {allowed_movements}")
+	#print(description.get(playerPosition, "not valid"))
+	print(description)
+	print(f"\nPossible exits: {allowed_movements}")
 	response = input().lower()
 
 	if response in potential_movements:
